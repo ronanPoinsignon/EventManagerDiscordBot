c02d2941b11686b3f3c15dbeff081d5c91c527ab
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-05-31 22:14:04 +0200

    fix des valeurs inutiles

--- src/environnement/environnement.ts
+++ src/environnement/environnement.ts
@@ -2,7 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/',
   frontEndUrl: 'http://localhost:4200/',
-  keycloakURL: 'http://localhost:8080',
-  token: process.env['TOKEN']!,
-  clientId: "1508982800437608590"
+  keycloakURL: 'http://localhost:8080'
 };
