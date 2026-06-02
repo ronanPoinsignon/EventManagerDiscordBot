152add5511d292b3618f72b4c89fad6ccdaed70a
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-06-02 15:28:21 +0200

    suppression virgule

--- src/environnement/environnement-prod.ts
+++ src/environnement/environnement-prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:8000/',
   frontEndUrl: 'http://localhost:4200/',
-  keycloakURL: 'http://localhost:8080/',
+  keycloakURL: 'http://localhost:8080/'
 };
