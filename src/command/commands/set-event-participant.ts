612a258f81950211ec0aed920abf244965676ff3
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-06-05 05:20:19 +0200

    ajout espace

--- src/command/commands/set-event-participant.ts
+++ src/command/commands/set-event-participant.ts
@@ -2,7 +2,7 @@ import { Command } from '../command.js';
 import { SlashCommandBuilder } from 'discord.js';
 import { showSetEventParticipant } from '../actions/show-set-event-participant-modal.js';
 
-export const command =new Command(
+export const command = new Command(
   new SlashCommandBuilder().setName('update-participants').setDescription('Modifier les participants d\'un événement ou d\'un programme'),
   async (interaction) => {
     const modal = await showSetEventParticipant(interaction.user.id);
